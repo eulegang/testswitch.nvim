2275bff5dc7aee85a28ade8b086b7a3f5f03aa65
eulegang <eulegang@eulegang.dev>
2024-11-06 23:25:33 -0600

    Adding luacheck and tests

---
+++ lua/tests/init_spec.lua
@@ -0,0 +1,68 @@
+local stub = require("luassert.stub")
+local match = require("luassert.match")
+local mod = require("testswitch.init")
+
+local function file_exists(files)
+  return function(file)
+    for _, f in ipairs(files) do
+      if file == f then
+        return 1
+      end
+    end
+
+    return 0
+  end
+end
+
+describe("#toggle", function()
+  local e = stub(vim.cmd, "e")
+  local notify = stub(vim, "notify")
+
+  before_each(function()
+    e:clear()
+    notify:clear()
+  end)
+
+
+  describe("(with basic folder structure)", function()
+    before_each(function()
+      stub(vim.fn, "filereadable").invokes(file_exists({
+        "src/xyz.ts",
+        "src/xyz.test.ts",
+      }))
+    end)
+
+    it("should toggle to test when toggling from code", function()
+      stub(vim.fn, "expand").returns("src/xyz.ts")
+
+      mod.toggle()
+
+      assert.spy(e).was.called_with("src/xyz.test.ts")
+    end)
+
+    it("should toggle to code when toggling from test", function()
+      stub(vim.fn, "expand").returns("src/xyz.test.ts")
+
+      mod.toggle()
+
+      assert.spy(e).was.called_with("src/xyz.ts")
+    end)
+  end)
+
+  describe("(with missing test)", function()
+    before_each(function()
+      stub(vim.fn, "filereadable").invokes(file_exists({
+        "src/xyz.ts",
+      }))
+    end)
+
+    it("should not toggle to test but notify instead", function()
+      stub(vim.fn, "expand").returns("src/xyz.ts")
+
+      mod.toggle()
+
+      assert.spy(e).was.not_called_with("src/xyz.test.ts")
+      assert.spy(notify).was.called_with("can not find test file", vim.log.levels.ERROR)
+    end)
+  end)
+end)
